4183f1b37ece742d278cfd74710600c504eda428
Aarav Vaish <aaravvaish2004@gmail.com>
2025-08-08 02:11:05 +0530

    removed methods

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,5 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000',
-  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   withCredentials: true,
 });
